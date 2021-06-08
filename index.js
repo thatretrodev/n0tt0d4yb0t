9c0e3901ce7aba2b05183fefaf3bb06d051c843b
thatretrodev <thatretrodev@outlook.com>
2021-06-08 13:19:45 -0400

    Add some bot code

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+//const fs = require('fs');
+const Discord = require('discord.js');
 
+
+function start_bot() {
+	const client = new Discord.Client();
+	//var token = fs.readFileSync(process.env.BOT_TOKEN, 'utf8');
+	//console.log(token);
+
+	client.on('ready', () => {
+		console.log(`Logged in as ${client.user.tag}!`);
+	});
+
+	client.on('message', msg => {
+		if (msg.content === '!helloworld') {
+			msg.reply('Hello, World!');
+		}
+	});
+
+	client.login(process.env.BOT_TOKEN);
+}
+
+start_bot();
