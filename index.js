55a2f9ed396e797ddd9dfd22cbc4198cc9192758
thatretrodev <thatretrodev@outlook.com>
2021-06-08 14:06:16 -0400

    Add stuff to bot

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ function start_bot() {
 		console.log(`Logged in as ${client.user.tag}!`);
 	});
 
+	client.on('guildMemberAdd', member => {
+		var i = 0;
+		if (member.tag.includes("twitter.com") || member.tag.includes("youtube.com") || member.tag.includes("discord.gg")) { // TODO: Replace this with a system that uses a PostgreSQL database
+			member.guild.channels.get('channelID').send("A message for " + member.toString() + ": ***Not today, and not tomorrow.***");
+			member.guild.members.ban(member);
+		}
+		//member.guild.channels.get(member.guild.systemChannelID).send("Hello, " + member.toString() + "!");
+		//member.roles.add(message.guild.roles.cache.find(role => role.name === "Member")).catch(console.error);
+	});
+
 	client.on('message', msg => {
 		if (msg.content === '!helloworld') {
 			msg.reply('Hello, World!');
